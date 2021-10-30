d26f81b5e4e1b79f4c89dc79abc22abdd0eaddda
stdai1016 <stdai1016@gmail.com>
2021-10-30 11:17:27 +0800

    Initial models

---
+++ app/Keyword.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Keyword extends Model
+{
+    // no timestamps
+    public $timestamps = false;
+}
---
+++ app/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+    public const DELETED_AT = 'deleted_at';
+
+    // convert type
+    protected $casts = [
+        Post::CREATED_AT => 'datetime:D, d m Y H:i:s e',
+        Post::UPDATED_AT => 'datetime:D, d m Y H:i:s e',
+        Post::DELETED_AT => 'datetime:D, d m Y H:i:s e'
+    ];
+
+    // hidden attributes
+    protected $hidden = [
+        Post::DELETED_AT
+    ];
+}
